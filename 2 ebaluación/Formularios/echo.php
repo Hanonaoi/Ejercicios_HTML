dd873d408c32d585d1842bf95b5cf55f8f0bcc7f
AS3-1-23 <AS3-1-23@uni.lan>
2019-12-09 10:00:55 +0100

    09/12/2019

---
+++ 2 ebaluación/Formularios/echo.php
@@ -0,0 +1,13 @@
+<!-- como empieza cualquier codigo de php -->
+<?php
+
+/* un codigo simple seria */
+/* echo "Kaixo"; */
+
+/* en php todo lo que empiece por dolar es una variable. y entre "" tenemos firstname y lastname (escritos igual) */
+
+// echo "<h1>";  podemos hacer titulos de esta manera. //
+echo $_GET["firstname"];
+// echo "</h1>"; //
+echo "<br>";
+echo $_GET["lastname"];
