4e9a13109294f11e2b00a99bff2d0f511e8bec00
AS3-1-23 <AS3-1-23@uni.lan>
2019-12-11 10:05:14 +0100

    Formulario 11/12

--- 2 ebaluación/Formularios/echo.php
+++ 2 ebaluación/Formularios/echo.php
@@ -1,13 +1,44 @@
 <!-- como empieza cualquier codigo de php -->
-<?php
 
-/* un codigo simple seria */
+
+<!-- un codigo simple seria */
 /* echo "Kaixo"; */
 
 /* en php todo lo que empiece por dolar es una variable. y entre "" tenemos firstname y lastname (escritos igual) */
 
 // echo "<h1>";  podemos hacer titulos de esta manera. //
-echo $_GET["firstname"];
+//    echo $_GET["Izena"]; //
 // echo "</h1>"; //
-echo "<br>";
-echo $_GET["lastname"];
+//    echo "<br>";
+//    echo $_GET["Pasahitza"]; //
+
+
+// SI EXISTE IMPRIMELO SINO NO//
+//if (iseet($_GET["firstname"])) // -->
+
+
+<?php
+
+if (isset($_GET["Vivora"]))
+    echo $_GET["Vivora"];
+
+if (isset($_GET["Poo"]))
+    echo $_GET["Poo"];
+
+if (isset($_GET["Tigresa"]))
+    echo $_GET["Tigresa"];
+
+if (isset($_GET["gender"]))
+    echo $_GET["gender"];
+
+if (isset($_GET["firstname"]))
+    echo $_GET["firstname"];
+
+if (isset($_GET["lastname"]))
+    echo $_GET["lastname"];
+
+if (isset($_GET["Oracion"]))
+    echo $_GET["Oracion"];
+
+if (isset($_GET["Idiomas"]))
+    echo $_GET["Idiomas"];
