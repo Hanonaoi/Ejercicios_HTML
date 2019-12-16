2a31a132d5452916d3b49ccefc4df2cf25f384a8
AS3-1-23 <AS3-1-23@uni.lan>
2019-12-16 10:11:59 +0100

    fORMULARIO

---
+++ 2 ebaluación/Formularios/echo2.php
@@ -0,0 +1,34 @@
+<?php
+
+if (isset($_GET["firstname"]))
+    echo $_GET["firstname"];
+
+if (isset($_GET["lastname"]))
+    echo $_GET["lastname"];
+
+if (isset($_GET["PuntosMila"]))
+    echo $_GET["PuntosMila"];
+
+if (isset($_GET["PuntosAlba"]))
+    echo $_GET["PuntosAlba"];
+
+if (isset($_GET["PuntosAdara"]))
+    echo $_GET["PuntosAdara"];
+
+if (isset($_GET["Concursante 1"]))
+    echo $_GET["Concursante 1"];
+
+if (isset($_GET["Concursante 2"]))
+    echo $_GET["Concursante 2"];
+
+if (isset($_GET["Concursante 3"]))
+    echo $_GET["Concursante 3"];
+
+if (isset($_GET["Texto"]))
+    echo $_GET["Texto"];
+
+if (isset($_GET["Comunidades"]))
+    echo $_GET["Comunidades"];
+
+
+
