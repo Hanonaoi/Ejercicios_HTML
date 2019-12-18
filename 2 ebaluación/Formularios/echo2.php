053c8f353a3770ead606c33d7d2e8cbf5e8f1ab9
AS3-1-23 <AS3-1-23@uni.lan>
2019-12-18 08:51:06 +0100

    Formulario final

--- 2 ebaluación/Formularios/echo2.php
+++ 2 ebaluación/Formularios/echo2.php
@@ -3,32 +3,41 @@
 if (isset($_GET["firstname"]))
     echo $_GET["firstname"];
 
+echo "<br>";
 if (isset($_GET["lastname"]))
     echo $_GET["lastname"];
 
+echo "<br>";
+if (isset($_GET["Comunidades"]))
+        echo $_GET["Comunidades"];
+    
+echo "<br>";
+if (isset($_GET["Concursante1"]))
+    echo $_GET["Concursante1"];
+
+echo "<br>";
 if (isset($_GET["PuntosMila"]))
     echo $_GET["PuntosMila"];
 
+echo "<br>";
+if (isset($_GET["Concursante2"]))
+    echo $_GET["Concursante2"];
+
+echo "<br>";
 if (isset($_GET["PuntosAlba"]))
     echo $_GET["PuntosAlba"];
 
+echo "<br>";
+if (isset($_GET["Concursante3"]))
+    echo $_GET["Concursante3"];
+
+echo "<br>";
 if (isset($_GET["PuntosAdara"]))
     echo $_GET["PuntosAdara"];
 
-if (isset($_GET["Concursante 1"]))
-    echo $_GET["Concursante 1"];
-
-if (isset($_GET["Concursante 2"]))
-    echo $_GET["Concursante 2"];
-
-if (isset($_GET["Concursante 3"]))
-    echo $_GET["Concursante 3"];
-
+echo "<br>";
 if (isset($_GET["Texto"]))
     echo $_GET["Texto"];
-
-if (isset($_GET["Comunidades"]))
-    echo $_GET["Comunidades"];
 
 
 
