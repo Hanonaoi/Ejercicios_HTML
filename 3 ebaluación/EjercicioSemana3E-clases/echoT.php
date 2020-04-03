b2743c8d357c00a46c26550f1be322bb5f7d8e0f
Hanonaoi <56817017+Hanonaoi@users.noreply.github.com>
2020-04-04 00:12:34 +0200

    Add files via upload

---
+++ 3 ebaluación/EjercicioSemana3E-clases/echoT.php
@@ -0,0 +1,100 @@
+Usuario
+Contraseña
+Recuerdame
+Vecinas
+Veneno
+Aramis
+Polo
+Esteban
+Saavedra
+Verka
+Text
+Año
+Puntuación
+Sirenita
+Famous
+email
+<?php
+
+if (isset($_GET["Usuario"]))
+    echo $_GET["Usuario"];
+
+   
+
+    if (isset($_GET["Contraseña"]))
+    echo $_GET["Contraseña"];
+
+
+    
+    if (isset($_GET["email"]))
+    echo $_GET["email"];
+
+  
+
+    if (isset($_GET["Recuerdame"]))
+    echo $_GET["Recuerdame"];
+
+
+
+    if (isset($_GET["Vecinas"]))
+    echo $_GET["Vecinas"];
+
+
+
+    if (isset($_GET["Veneno"]))
+    echo $_GET["Veneno"];
+
+
+
+    if (isset($_GET["Aramis"]))
+    echo $_GET["Aramis"];
+
+
+
+    if (isset($_GET["Polo"]))
+    echo $_GET["Polo"];
+
+
+
+    if (isset($_GET["Esteban"]))
+    echo $_GET["Esteban"];
+
+
+
+    if (isset($_GET["Saavedra"]))
+    echo $_GET["Saavedra"];
+
+  
+
+    if (isset($_GET["Verka"]))
+    echo $_GET["Verka"];
+
+    
+
+    if (isset($_GET["Text"]))
+    echo $_GET["Text"];
+
+    
+
+    if (isset($_GET["Año"]))
+    echo $_GET["Año"];
+
+
+
+    if (isset($_GET["Puntuación"]))
+    echo $_GET["Puntuación"];
+
+ 
+
+    if (isset($_GET["Sirenita"]))
+    echo $_GET["Sirenita"];
+
+
+
+    if (isset($_GET["Famous"]))
+    echo $_GET["Famous"];
+
+ 
+
+  
+    
